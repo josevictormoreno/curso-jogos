4e767324478a80bd11cb40221604eec66aa4ea8c
josevictormoreno <josevictor.admoreno@gmail.com>
2023-02-25 20:59:09 -0300

    completa codigo da aula 10

--- aula10/main.cpp
+++ aula10/main.cpp
@@ -10,9 +10,12 @@ int main()
   rectangle.setSize(sf::Vector2f(100.f, 100.f));
   rectangle.setFillColor(sf::Color::Green);
   window.setFramerateLimit(144);
-
   float pos_rectangle = static_cast<float>(std::experimental::randint(10, (int)(window.getSize().x - rectangle.getSize().x - 10)));
   rectangle.setPosition(sf::Vector2(pos_rectangle, 0.f));
+  sf::Vector2i pos_mouse;
+  sf::Vector2f coord_mouse;
+  int score = 0;
+
   while (window.isOpen())
   {
     sf::Event event;
@@ -20,6 +23,20 @@ int main()
     {
       if (event.type == sf::Event::Closed)
         window.close();
+
+      pos_mouse = sf::Mouse::getPosition(window);
+      coord_mouse = window.mapPixelToCoords(pos_mouse);
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+      if (rectangle.getGlobalBounds().contains(coord_mouse))
+      {
+        pos_rectangle = static_cast<float>(std::experimental::randint(10, (int)(window.getSize().x - rectangle.getSize().x - 10)));
+        rectangle.setPosition(sf::Vector2(pos_rectangle, 0.f));
+        score++;
+        std::cout << "Score: " << score << '\n';
+      }
     }
 
     rectangle.move(0.f, 3.9f);
