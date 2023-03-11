1ac0b09e4ed153a8fc7a5d6302e52e16031d25b0
josevictormoreno <josevictor.admoreno@gmail.com>
2023-03-11 01:37:27 -0300

    aula 19 e 21 criação do jogo snake

---
+++ jogo02/main.cpp
@@ -0,0 +1,9 @@
+#include "snake.hpp"
+
+int main(){
+  Snake * snake = new Snake();
+  snake->run_game();
+  delete snake;
+  snake = nullptr;
+  return EXIT_SUCCESS;
+}
---
+++ jogo02/todo.md
@@ -0,0 +1,4 @@
+## Todo list
+- Adicionar vidas
+- Adicionar condições de direção
+- Adicionar colisão com o proprio objeto
